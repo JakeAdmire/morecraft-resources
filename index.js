9c21d8668115fc121443d59902f9c042b81f6b95
Jake Admire <jakeadmire1@gmail.com>
2024-03-27 16:55:45 -0600

    Run tests and flesh out logic further

--- index.js
+++ index.js
@@ -14,20 +14,54 @@ async function init() {
   server = await getServerByName('wpMoreCraft')
 
   // 1. Bundle updated resource pack
-  const fileSize = await bundleTexturePack('texturePack')
-  console.log(`A new texture pack has been bundled at ${ fileSize }`)
+  console.log('Bundling updated resource pack...')
+  const { fileSize, outputFilename, outputPath } = await bundleTexturePack('texturePack')
+  console.log(`A new ${ fileSize } resource pack has been bundled at "${ outputPath }"`)
+  console.log('---')
 
   // 2. Get current hash from server.properties / resource-pack-sha1
+  console.log('Checking for an existing "texture-pack-sha1" hash...')
+
+  const oldHash = await getFileConfigValue('server.properties', 'resource-pack-sha1')
+  if (oldHash) {
+    console.log(`Found an existing "resource-pack-sha1" value of "${ oldHash }"`)
+  } else {
+    console.log('No existing "resource-pack-sha1" value found')
+  }
+  console.log('---')
 
   // 3. Get new hash based on the updated texture pack archive
+  console.log('Generating a new hash from the newly bundled resource pack...')
   const newHash = await getHashFromFile('texturePack.zip');
-  console.log(`Generated the following hash to replace "texture-pack-sha1": "${ newHash }"`)
-
+  console.log(`Generated the following hash to replace "resource-pack-sha1": "${ newHash }"`)
+  
   // 4. Compare the new and old hashes
+  if (oldHash && oldHash == newHash) console.log('⚠️ Warning: The new & old hash match')
+  
+  console.log('---')
   
   // 5. Update server.properties
-  //  a. resource-pack
+  // a. resource-pack
+  if (outputPath) {
+    console.log('Updating the "resource-pack" value...')
+
+    const idealPath = `https://github.com/JakeAdmire/morecraft-resources/raw/main/buid/${ outputFilename }`
+
+    await setFileConfigValue('server.properties', 'resource-pack', outputPath)
+
+    console.log('Updated the value of "resource-pack" and saved the server.properties config')
+    console.log('---')
+  }
+
   //  b. resource-pack-sha1
+  if (newHash) {
+    console.log('Updating the "resource-pack-sha1" value...')
+
+    await setFileConfigValue('server.properties', 'resource-pack-sha1', newHash)
+
+    console.log('Updated the value of "resource-pack-sha1" and saved the server.properties config')
+    console.log('---')
+  }
 
   // 6. Wait for the next restart? 
   // How do we force it to reload without restarting the server? 
@@ -35,8 +69,30 @@ async function init() {
 
 // * Utilities
 
+async function getFileConfigValue(fileName, optionName) {
+  let file = server.getFile(fileName);
+  let config = file.getConfig();
+  let options = await config.getOptions();
+  
+  const optionValue = options.get(optionName).getValue()
+
+  return optionValue
+}
+
+async function setFileConfigValue(fileName, optionName, newValue) {
+  let file = server.getFile(fileName);
+  let config = file.getConfig();
+  let options = await config.getOptions();
+
+  options.get(optionName).setValue(newValue)
+
+  await config.save();
+}
+
 const bundleTexturePack = (directory = 'texturePack') => new Promise((resolve, reject) => {
-  const output = createWriteStream(__dirname + '/texturePack.zip');
+  const outputFilename = `${Date.now()}.zip`
+  const outputPath = `${__dirname}/build/${outputFilename}`
+  const output = createWriteStream(outputPath);
   const archive = archiver('zip');
 
   archive.directory(directory, false);
@@ -46,7 +102,7 @@ const bundleTexturePack = (directory = 'texturePack') => new Promise((resolve, r
     const bytes = archive.pointer()
     const megabytes = Math.round((bytes / 1000000) * 10) / 10
 
-    resolve(`${ megabytes }mb`)
+    resolve({ fileSize: `${ megabytes }mb`, outputFilename, outputPath })
   });
 
   archive.finalize();
